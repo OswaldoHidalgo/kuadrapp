07968588d989eba074fd9dc6a81b8d67b3c6b544
Oswaldo Hidalgo <tu_correo@example.com>
2026-09-08 23:17:04 -0400

    fix: replace with real valid supabase anon key

--- src/database/supabase.ts
+++ src/database/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://wuveugddohtuleysuzrr.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'PeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1dmV1Z2Rkb2h0dWxleXN1enJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5MDIxNjMsImV4cCI6MjEwNDQ3ODE2M30.8WS9x-0do-SJ6wWZF4MWG8082iO8HeqKRiReDm3cMSY'; 
+const SUPABASE_PUBLISHABLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1dmV1Z2Rkb2h0dWxleXN1enJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5MDIxNjMsImV4cCI6MjEwNDQ3ODE2M30.8WS9x-0do-SJ6wWZF4MWG8082iO8HeqKRiReDm3cMSY'; 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
